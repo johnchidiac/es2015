aeb43da5812c27ea92f8cd3784e1a4ef8a2d95af
John Chidiac <johnchidiac@seneca.local>
2023-09-06 16:39:51 -0400

    First commit

---
+++ es2015.js
@@ -0,0 +1,26 @@
+
+/*
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+*/
+
+const PI = 3.14;
+
+/*
+- What is the difference between ***var*** and ***let***?
+
+let has block-level scoping, var has function-level scoping
+
+- What is the difference between **var** and ***const***?
+
+var can be reassigned and redeclared, const cannot be reassigned or redeclared
+
+- What is the difference between **let** and **const**?
+
+let can be reassigned, const cannot
+
+- What is hoisting?
+
+hoisting brings awareness of a variable declared with var to the top of its scope. It will return undefined (type error) whereas let or const will return reference errors if called before initialized
+
+*/
